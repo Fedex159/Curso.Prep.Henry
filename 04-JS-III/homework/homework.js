b5780d4e36144eda6a7275e3c11acb673bb94333
unknown <fede.gallina94@gmail.com>
2021-06-25 14:00:54 -0300

    Fix empiezaConNueve negative numbers

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -165,6 +165,9 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
+  if (n < 0) {
+    n = n * -1;
+  }
   if (n === 9) {
     return true;
   } else {
